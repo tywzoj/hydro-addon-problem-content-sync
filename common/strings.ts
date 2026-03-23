82e359445deaf281dd02eabf1eb1936c887fefd4
Antares <antares.mi@outlook.com>
2026-03-23 21:57:35 +0800

    remove fallback

--- common/strings.ts
+++ common/strings.ts
@@ -23,14 +23,6 @@ const strings: Record<string, Record<CE_StringKey, string>> = {
         [CE_StringKey.EditReferredProblem]: "編輯被引用的題目",
         [CE_StringKey.ConfirmEditReferredProblem]: "您不能編輯被引用的題目。是否要查看原始題目？",
     },
-    en: {
-        [CE_StringKey.Title]: "Problem Content Sync",
-        [CE_StringKey.AllowDistributeProblemChange]: "Allow distribute problem change to other domains",
-        [CE_StringKey.DisableEditReferredProblem]: "Disable edit referred problem",
-        [CE_StringKey.EditReferredProblem]: "Edit referred problem",
-        [CE_StringKey.ConfirmEditReferredProblem]:
-            "You can not edit a referred problem. Do you want to view the original problem instead?",
-    },
 };
 
 export function loadStrings(ctx: Context) {

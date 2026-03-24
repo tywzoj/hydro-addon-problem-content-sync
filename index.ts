e5ac17d4915b13246aa8fdecf91e8ada56336912
Antares <antares.mi@outlook.com>
2026-03-24 18:12:58 +0800

    fix

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import type { Context } from "hydrooj";
 import { loadStrings } from "./common/strings";
 import { applyDisableEditReferred } from "./feature/disable-edit-referred";
 import { applyDistributeChange } from "./feature/distribute-change";
+import { applyProblemContentSync } from "./feature/problem-content-sync";
 import { applySharedUiContext } from "./feature/shared-ui-context";
 
 export { Config } from "./feature/config";
@@ -10,6 +11,8 @@ export { Config } from "./feature/config";
 export function apply(ctx: Context) {
     loadStrings(ctx);
     applySharedUiContext(ctx);
+
+    applyProblemContentSync(ctx);
     applyDisableEditReferred(ctx);
     applyDistributeChange(ctx);
 }

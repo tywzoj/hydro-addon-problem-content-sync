8e947935d2a8e2957585f137b264e3d665b49a16
Antares <antares.mi@outlook.com>
2026-03-24 14:49:49 +0800

    fix

--- index.ts
+++ index.ts
@@ -2,10 +2,12 @@ import type { Context } from "hydrooj";
 
 import { loadStrings } from "./common/strings";
 import { applyDisableEditReferred } from "./feature/disable-edit-referred";
+import { applyDistributeChange } from "./feature/distribute-change";
 
 export { Config } from "./feature/config";
 
 export function apply(ctx: Context) {
     loadStrings(ctx);
     applyDisableEditReferred(ctx);
+    applyDistributeChange(ctx);
 }

5004d654b9210ee5035845ce3c2868e52a0f2201
Antares <antares.mi@outlook.com>
2026-03-24 15:12:12 +0800

    fix

--- feature/shared-ui-context.ts
+++ feature/shared-ui-context.ts
@@ -1,15 +1,22 @@
 import type { Context } from "hydrooj";
-import type { ProblemDetailHandler } from "hydrooj/src/handler/problem";
+import type { ProblemDetailHandler, ProblemEditHandler } from "hydrooj/src/handler/problem";
 
 import { getUiContext } from "../common/utils";
 
 export function applySharedUiContext(ctx: Context) {
-    ctx.on("handler/after/ProblemDetail#get", (handler: ProblemDetailHandler) => {
+    const fn = (handler: ProblemDetailHandler) => {
+        const uiContext = getUiContext(handler);
         if (handler.pdoc.reference) {
-            getUiContext(handler).originalProblemUrl = handler.url("problem_detail", {
+            uiContext.isOriginalProblem = false;
+            uiContext.originalProblemUrl = handler.url("problem_detail", {
                 domainId: handler.pdoc.reference.domainId,
                 pid: handler.pdoc.reference.pid,
             });
+        } else {
+            uiContext.isOriginalProblem = true;
         }
-    });
+    };
+
+    ctx.on("handler/after/ProblemDetail#get", (handler: ProblemDetailHandler) => fn(handler));
+    ctx.on("handler/after/ProblemEdit#get", (handler: ProblemEditHandler) => fn(handler));
 }

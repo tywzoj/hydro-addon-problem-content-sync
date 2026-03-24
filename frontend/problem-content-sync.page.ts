1bd88397bea8841dbcf762589cc1b81eb95fff48
Antares <antares.mi@outlook.com>
2026-03-24 19:05:09 +0800

    fix

--- frontend/problem-content-sync.page.ts
+++ frontend/problem-content-sync.page.ts
@@ -10,27 +10,21 @@ addPage(
         if (!isOriginalProblem) {
             const editItem = $(".menu__item").has('a[href*="/edit"]');
             if (editItem.length > 0) {
-                $(`
-                    <li class="menu__item">
-                        <form method="post" action="${editItem.find("a").attr("href")}">
-                            <input type="hidden" name="operation" value="${OPER_SYNC_WITH_ORIGINAL_PROBLEM}">
-                            <button type="submit" class="menu__link" name="problem-sidebar__${OPER_SYNC_WITH_ORIGINAL_PROBLEM}">
-                                <span class="icon icon-refresh"></span> ${i18n(CE_StringKey.SyncWithOriginalProblem)}
-                            </button>
-                        </form>
-                    </li>
-                `).insertAfter(editItem);
+                const form = $(`
+                    <form method="post" action="${editItem.find("a").attr("href")}">
+                        <input type="hidden" name="operation" value="${OPER_SYNC_WITH_ORIGINAL_PROBLEM}">
+                        <button type="submit" class="menu__link">
+                            <span class="icon icon-refresh"></span> ${i18n(CE_StringKey.SyncWithOriginalProblem)}
+                        </button>
+                    </form>
+                `).on("submit", (ev) => {
+                    ev.preventDefault();
+                    void confirm(i18n(CE_StringKey.ConfirmSyncWithOriginalProblem)).then((yes) => {
+                        if (yes) $(ev.currentTarget).off("submit").trigger("submit");
+                    });
+                });
 
-                $(document).on(
-                    "submit",
-                    `form button[name="problem-sidebar__${OPER_SYNC_WITH_ORIGINAL_PROBLEM}"]`,
-                    (ev) => {
-                        ev.preventDefault();
-                        void confirm(i18n(CE_StringKey.ConfirmSyncWithOriginalProblem)).then((yes) => {
-                            if (yes) $(ev.currentTarget).closest("form").trigger("submit");
-                        });
-                    },
-                );
+                $('<li class="menu__item">').add(form).insertAfter(editItem);
             }
         }
     }),
